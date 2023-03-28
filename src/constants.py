03aa6f44b83e59f21229c255d8b1803869aa8b63
Kattson Bastos <kattsonbastos@gmail.com>
2023-03-28 23:59:15 +0000

    feat: adding project and topics constants

--- src/constants.py
+++ src/constants.py
@@ -5,4 +5,8 @@ class Constants(Enum):
     data_input_path = "gs://practcing-beam/input/"
     data_output_path = "gs://practcing-beam/output/"
 
+    gcp_project_name = 'k-practices'
+    gcp_user_input_topic = 'user-input'
+    user_api_url = 'https://randomuser.me/api'
+
     serviceAccount = r'/tmp/beam-key.json'
